25e2d4a56743e6b3bb4a404f05ba9dbec676e3bf
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-25 23:33:00 +0530

    remove the emailjs dependency

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,5 @@
 import { useState,useRef } from 'react';
 import { Mail, Phone, MapPin, Github, Linkedin, ExternalLink } from 'lucide-react';
-import emailjs from 'emailjs-com';
 import {toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
